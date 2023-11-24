42a998e77534e1e17f737c0567f73959e6976c4a
Guillermo Muñoz <guillemunozi2003@gmail.com>
2023-11-24 12:37:01 -0500

    Summary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html','./Pages/about-us.html','./Pages/contact-us.html','./Pages/location.html','./Pages/menu.html'],
+  content: [
+    './index.html',
+    './Pages/about-us.html',
+    './Pages/contact-us.html',
+    './Pages/location.html',
+    './Pages/menu.html'
+  ],
   theme: {
     extend: {},
   },

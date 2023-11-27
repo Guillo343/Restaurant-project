c479a0afca4f026df8f26498600560515965e60d
Guillermo Muñoz <guillemunozi2003@gmail.com>
2023-11-27 12:59:41 -0500

    Summary
    Some coding practice. I need to make it responsive, make the gallery section and the menu

---
+++ location.js
@@ -0,0 +1,8 @@
+function initMap(){
+    var options = {
+        zoom:8,
+        center:{lat:25.761681,lng:-80.191788 }
+    }
+    var map = new 
+google.maps.Map(document.getElementById('map'), options)
+}
